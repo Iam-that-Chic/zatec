cc895c335081834359f7c650084b4e015acd330d
Pamela Muzoma <pamelamuzoma@gmail.com>
2023-04-03 16:54:23 +0200

    bootstrap new design that is responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.5/dist/sweetalert2.min.css">
 
     <title>ZATEC</title>
   </head>
@@ -44,10 +45,9 @@
     </div>
       
 </body>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-
-
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.5/dist/sweetalert2.min.js"></script>
+@yield('scripts')
 </html>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -6,27 +6,27 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse2">
             <div class="navbar-nav">
-                <a href="{{ url('/') }}" class="nav-item nav-link active">Home</a>
-                <a href="{{ route('dashboard') }}" class="nav-item nav-link">Dashboard</a>
+                <a href="{{ url('/') }}" class="nav-item nav-link {{ request()->routeIs('home') ? 'active' : '' }}">Dashboard</a>     
                 <div class="nav-item dropdown">
                   <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Favorites</a>
                   <div class="dropdown-menu">
-                      <a href="{{ route('favartists') }}" class="dropdown-item">Artists</a>
-                      <a href="{{ route('favalbums') }}" class="dropdown-item">Albums</a>
+                      <a href="{{ route('favartists') }}" class="dropdown-item {{ request()->routeIs('favartists') ? 'active' : '' }}">Artists</a>
+                      <a href="{{ route('favalbums') }}" class="dropdown-item {{ request()->routeIs('favalbums') ? 'active' : '' }}">Albums</a>
                   </div>
                 </div>
-                @if (Auth::check())
+                <a href="{{ url('/') }}" class="nav-item nav-link {{ request()->routeIs('search') ? 'active' : '' }}" >Search</a>
+            </div>
+            @if (Auth::check())
                    <form method="POST" action="{{ route('logout') }}">
                     @csrf
-                    <a href="route('logout')" class="nav-item nav-link"
+                    <a href="route('logout')" class="nav-item nav-link btn-danger"
                         onclick="event.preventDefault();
                                 this.closest('form').submit();">
                                 <span>Log Out</span>
-                            </a>
+                   </a>
                 </form>  
                 @endif
-               
-            </div>
         </div>
+
     </div>        
   </nav>
